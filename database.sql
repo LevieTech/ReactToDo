f41322d7896ca9566f3954e7858fe5ae707a5189
Leigh Stephenson <Leighh.stephenson@gmail.com>
2023-08-24 22:14:27 -0500

    Added sample data to sql file, a few minor changes to user things in server

--- database.sql
+++ database.sql
@@ -1,26 +1,32 @@
--- CREATE DATABASE "react-to-do"
+-- database name = react-to-do
 
--- USER is a reserved keyword with Postgres
--- You must use double quotes in every query that user is in:
--- ex. SELECT * FROM "user";
--- Otherwise you will have errors!
-CREATE TABLE "user" (
+CREATE TABLE "users" (
     "id" SERIAL PRIMARY KEY,
     "username" VARCHAR (80) UNIQUE NOT NULL,
     "password" VARCHAR (1000) NOT NULL,
     "access_level" INT DEFAULT 0
 );
+-- register a user before proceeding
 
 -- (task name, date added, due date, priority level, notes, completion status )
-
 CREATE TABLE tasklist (
     "id" serial PRIMARY KEY,
+    "user_id" INT REFERENCES users,
     "taskname" VARCHAR (400) NOT NULL,
     "dateadded" VARCHAR (50),
     "duedate" VARCHAR (50),
-    "completionstatus" BOOLEAN DEFAULT FALSE
-    "prioritylevel" VARCHAR (10)
+	"prioritylevel" VARCHAR (10),
+    "completionstatus" BOOLEAN DEFAULT FALSE,
     "notes" VARCHAR (2000)
 );
 
--- need to add sample data and sample alert messages 
+INSERT INTO "tasklist" ("id", "user_id", "taskname", "dateadded", "duedate", "prioritylevel", "completionstatus", "notes")
+
+VALUES ('1', '1', 'Schedule appointment', '7/19/2023', '8/12/2023', 'medium', 'FALSE', 'Try to get an appointment after lunch'),
+('2', '1', 'Call Grandma', '7/26/2023', '8/1/2023', 'high', 'FALSE', 'She has been waiting for me to call'),
+('3', '1', 'Drop off returns', '8/2/2023', '9/25/2023', 'low', 'FALSE', 'Make sure you bring the receipt'),
+('4', '1', 'Order new product', '8/13/2023', '8/25/2023', 'medium', 'TRUE', 'Order before it sells out!'),
+('5', '1', 'Do laundrey', '8/20/2023', '8/23/2023', 'high', 'TRUE', 'Separate colors')
+;
+
+-- need to add sample alert messages and table

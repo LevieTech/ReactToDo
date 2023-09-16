bfc0a42006b41726c6d73baf9abf43b9ddaf7076
Sam Gossie <sjg3609@gmail.com>
2023-09-16 00:16:33 -0500

    sams-little-fun-changes

--- src/components/Tasks/MyTasks.jsx
+++ src/components/Tasks/MyTasks.jsx
@@ -49,63 +49,64 @@ function MyTasks() {
     }
 
     return (
-        <div className="my-tasks-container">
-            <h2 className="solid">Hey {user.username}! Lets Get Started!</h2>
+        <center>
+            <div className="my-tasks-container">
+                <h2 className="solid">Hey {user.username}! Lets Get Started!</h2>
+                <Button
+                    component={Link} to="/add_task"
+                    className="addtaskbutton"
+                    variant="contained"
+                    style={{
+                        backgroundColor: 'hsl(94, 82%, 60%)',
+                        color: 'white',
+                        fontFamily: "Georgia",
+                        textShadow: '4px 1px 2px rgba(0, 0, 0, 0.8)',
+                        fontSize: '23px',
+                        fontWeight: 'bold',
+                    }}
+                >Add a Task
+                </Button>
 
-            <Button
-                component={Link} to="/add_task"
-                className="addtaskbutton"
-                variant="contained"
-                style={{
-                    backgroundColor: 'hsl(94, 82%, 60%)',
-                    color: 'white',
-                    fontFamily: "Georgia",
-                    textShadow: '4px 1px 2px rgba(0, 0, 0, 0.8)',
-                    fontSize: '23px',
-                    fontWeight: 'bold',
-                }}
-            >Add a Task
-            </Button>
+                <h2 className="saved"
+                    style={{
+                        fontFamily: "Georgia",
+                        textShadow: '1px 1px 2px rgba(0, 0, 0, 0.8) 2px 2px 3px white',
+                        fontSize: '40px',
+                        fontWeight: 'bold',
+                    }}
+                >Saved Tasks:</h2>
 
-            <h2 className="saved"
-                style={{
-                    fontFamily: "Georgia",
-                    textShadow: '1px 1px 2px rgba(0, 0, 0, 0.8) 2px 2px 3px white',
-                    fontSize: '40px',
-                    fontWeight: 'bold',
-                }}
-            >Saved Tasks:</h2>
+                {task.length === 0 ? (
+                    <p>No saved tasks to show...yet!</p>
+                ) : (
+                    task.map((task, i) => (
+                        <EachTask
+                            key={i}
+                            task={{
+                                id: task.id,
+                                taskname: task.taskname,
+                                dateadded: task.dateadded,
+                                duedate: task.duedate,
+                                prioritylevel: task.prioritylevel,
+                                completionstatus: task.completionstatus,
+                                notes: task.notes,
+                            }}
+                            handleEditTask={handleEditTask}
+                            handleDeleteTask={() => handleDeleteTask(null, task.id)}
+                            savedTasks={task}
+                            style={{
+                                fontFamily: "Georgia",
+                                fontWeight: "bolder",
+                                textShadow: '4px 1px 2px rgba(0, 0, 0, 0.8)',
+                                fontSize: '30px',
+                            }}
+                        />
+                    ))
+                )}
 
-            {task.length === 0 ? (
-                <p>No saved tasks to show...yet!</p>
-            ) : (
-                task.map((task, i) => (
-                    <EachTask
-                        key={i}
-                        task={{
-                            id: task.id,
-                            taskname: task.taskname,
-                            dateadded: task.dateadded,
-                            duedate: task.duedate,
-                            prioritylevel: task.prioritylevel,
-                            completionstatus: task.completionstatus,
-                            notes: task.notes,
-                        }}
-                        handleEditTask={handleEditTask}
-                        handleDeleteTask={() => handleDeleteTask(null, task.id)}
-                        savedTasks={task}
-                        style={{
-                            fontFamily: "Georgia",
-                            fontWeight: "bolder",
-                            textShadow: '4px 1px 2px rgba(0, 0, 0, 0.8)',
-                            fontSize: '30px',
-                        }}
-                    />
-                ))
-            )}
-
-            <Route path="/add_task/:taskId" component={AddTask} />
-        </div>
+                <Route path="/add_task/:taskId" component={AddTask} />
+            </div>
+        </center>
     );
 }
 
